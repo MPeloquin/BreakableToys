1230729416745b2e00892064b2e44958b070e518
Maxime Péloquin <maxime.peloquin@gmail.com>
2017-10-16 20:39:58 -0400

    Complete module 12

--- ReactPython/frontend/src/components/course/ManageCoursePage.js
+++ ReactPython/frontend/src/components/course/ManageCoursePage.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
+
 
 class ManageCoursePage extends React.Component {
     constructor(props, context) {
@@ -10,7 +12,8 @@ class ManageCoursePage extends React.Component {
 
         this.state = {
             course: Object.assign({}, this.props.course),
-            errors: {}
+            errors: {},
+            saving: false
         };
 
         this.updateCourseState = this.updateCourseState.bind(this);
@@ -32,20 +35,32 @@ class ManageCoursePage extends React.Component {
 
     saveCourse(event) {
         event.preventDefault();
-        this.props.actions.saveCourse(this.state.course);
-        this.context.router.push('/courses');
+        this.setState({saving: true});
+        this.props.actions
+            .saveCourse(this.state.course)
+            .then(() => {
+                toastr.success('Course saved');
+                this.setState({saving: false});
+                this.context.router.push('/courses');
+            })
+            .catch((error) => {
+                toastr.error(error);
+                this.setState({saving: false});
+            });
     }
 
     render() {
         return (
             <div>
                 <h1>Manage Course</h1>
+
                 <CourseForm 
                     allAuthors={this.props.authors}
                     onChange={this.updateCourseState}
                     onSave={this.saveCourse}
                     course={this.state.course}
                     errors={this.state.errors}
+                    saving={this.state.saving}
                 />
             </div>
         );
