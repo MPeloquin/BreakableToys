91d8a838138bb3b053920ef98b9a89401e933052
Maxime Péloquin <maxime.peloquin@gmail.com>
2018-04-24 22:37:21 -0400

    Spawn enemies and display countdown text

---
+++ Unity/TowerDefense/Assets/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPreFab;
+    public float timeBetweenWaves = 5f;
+    public Transform spawnPoint;
+    public Text wavecountDownText;
+
+    private float countDown = 2f;
+    private int waveNumber = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update ()
+    {
+        if (countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+        wavecountDownText.text = countDown.ToString("####");
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        waveNumber++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPreFab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
