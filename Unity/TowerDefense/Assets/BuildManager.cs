5ebea5843eb8eb51f7e5d661c77bdc7911fa83f0
Maxime Péloquin <maxime.peloquin@gmail.com>
2018-04-29 18:37:26 -0400

    Allow to build new turrets

---
+++ Unity/TowerDefense/Assets/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Persistence;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Unity/TowerDefense/Assets/Node.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+
+    private GameObject turret;
+    private Renderer rend;
+    private Color startColor;
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            print("Can't build here");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+	void Start ()
+	{
+	    rend = GetComponent<Renderer>();
+	    startColor = rend.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
